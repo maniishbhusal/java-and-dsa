98265ef6bcd47ea268dd1d942ca84d532bbb7a5a
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-01 12:04:32 +0545

    finish variables and data types section

--- 1. Variables & Data Types/Input.java
+++ 1. Variables & Data Types/Input.java
@@ -2,15 +2,41 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
+        // Taking Input from user
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * String input = sc.nextLine();
+         * System.out.println(input);
+         * 
+         * int num = sc.nextInt();
+         * System.out.println(num);
+         * 
+         * long phoneNum = sc.nextLong();
+         * System.out.println(phoneNum);
+         */
+
+        // Sum of two numbers
+        // Scanner sc = new Scanner((System.in));
+        // try {
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("The sum of " + a + " and " + b + " is " + sum);
+        // } finally {
+        // sc.close();
+        // }
+
+        // Area of a Circle
+        float pie = 3.14f;
         Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        System.out.println(input);
+        try {
+        float radius = sc.nextFloat();
+        float area = pie * radius * radius;
+        System.out.println("The radius of a area is " + area);
+        } finally {
+        sc.close();
+        }
 
-        int num = sc.nextInt();
-        System.out.println(num);
-
-        long phoneNum = sc.nextLong();
-        System.out.println(phoneNum);
     }
 
 }
--- 1. Variables & Data Types/JavaBasics.java
+++ 1. Variables & Data Types/JavaBasics.java
@@ -2,5 +2,12 @@ public class JavaBasics {
 
     public static void main(String args[]) {
         System.out.println("Hello World!");
+        byte a = 2;
+        // byte b = (byte) (a * 112);
+        int b = a * 2;
+        float c = 2f;
+        double d = 1;
+        double e = a + b + c + d;
+        System.out.println(e);
     }
 }
