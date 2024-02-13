e2f99d3c9097a0f751f286df5c1cb8b7a3a93ba4
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-13 10:32:31 +0545

    learn about Insertion Sort

---
+++ 6. Sorting/InsertionSort.java
@@ -0,0 +1,24 @@
+public class InsertionSort {
+
+    public static void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = temp;
+        }
+
+        // print sorted array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 2, 1 };
+        insertionSort(arr);
+    }
+}
