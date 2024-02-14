d319b86d2d08c03f99282f626c82bca6b983cc04
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-14 20:43:49 +0545

    learn to solve spiral matrix problem

---
+++ 7. 2D Arrays/SpiralMatrix.java
@@ -0,0 +1,59 @@
+/*
+ 1  2  3  4
+ 5  6  7  8
+ 9 10 11 12
+ 13 14 15 16
+ */
+
+public class SpiralMatrix {
+
+    public static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length - 1;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+            startRow++;
+
+            // right
+            for (int i = startRow; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+            endCol--;
+
+            // bottom
+            if (startRow <= endRow) {
+                for (int j = endCol; j >= startCol; j--) {
+                    System.out.print(matrix[endRow][j] + " ");
+                }
+            }
+            endRow--;
+
+            // left
+            if (startCol <= endCol) {
+                for (int i = endRow; i >= startRow; i--) {
+                    System.out.print(matrix[i][startCol] + " ");
+                }
+            }
+            startCol++;
+
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 }
+        };
+        printSpiral(matrix);
+    }
+}
