4ce9dee65474064ae6c544e9c77b328995f961e7
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-15 11:08:19 +0545

    solve staircase search problem

---
+++ 7. 2D Arrays/StairCaseSearch.java
@@ -0,0 +1,30 @@
+// Search for a key in row wise and col wise sorted matrix
+public class StairCaseSearch {
+
+    public static boolean stairCaseSearch(int matrix[][], int key) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == key) {
+                System.out.println("found key at (" + row + "," + col + ")");
+                return true;
+            } else if (key < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("key not found");
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {
+                { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 27, 29, 37, 48 },
+                { 32, 33, 39, 50 },
+        };
+        int key = 59;
+        stairCaseSearch(matrix, key);
+    }
+}
