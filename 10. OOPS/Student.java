0631a324d02f5e002ff595612dd88c729a860cae
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-17 21:00:13 +0545

    learn about constructor

--- 10. OOPS/Student.java
+++ 10. OOPS/Student.java
@@ -1,8 +1,29 @@
 public class Student {
 
     String name;
-    private int rollNo;
+    int rollNo;
     double percent;
+    final String school = "Pawan"; // now it can't be modified
+    private static int noOfStudents;
+
+    // default constructor
+    public Student() {
+        noOfStudents++;
+    }
+
+    // constructor
+    public Student(String name, int rollNo, double percent) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.percent = percent;
+        noOfStudents++;
+
+        System.out.println("this will be printed automatically when objects is created");
+    }
+
+    public static int getNoOfStudents() {
+        return noOfStudents;
+    }
 
     // getter
     public int getRollNo() {
--- 10. OOPS/StudentClass.java
+++ 10. OOPS/StudentClass.java
@@ -1,37 +1,40 @@
 public class StudentClass {
 
-    // Car class
-    public static class Car {
-
-        String name;
-        String color;
-        int wheel;
-    }
-
     // function that accepts Class
-    public static void student(Student x) {
-        x.name = "Grishma";
-    }
+    // public static void student(Student x) {
+    // x.name = "Grishma";
+    // }
 
     public static void main(String[] args) {
-        Student x = new Student();
-        x.name = "Manish";
-        x.percent = 92.22;
-        // x.rollNo = 13;
-        System.out.println(x.getRollNo()); // getter function calling
-        x.setRollNo(10); // setter function calling
-        System.out.println(x.getRollNo());
+        // Student x = new Student();
+        // x.name = "Manish";
+        // x.percent = 92.22;
+        // // x.rollNo = 13;
+        // System.out.println(x.getRollNo()); // getter function calling
+        // x.setRollNo(10); // setter function calling
+        // System.out.println(x.getRollNo());
 
-        student(x);
+        // student(x);
 
-        Student x2 = new Student();
-        x2.name = "Manish Bhusal";
-        System.out.println(x2.name);
+        // Student x2 = new Student();
+        // x2.name = "Manish Bhusal";
+        // System.out.println(x2.name);
 
-        Car c1 = new Car();
-        c1.name = "Ferharri";
-        c1.color = "red";
+        // Car c1 = new Car();
+        // c1.name = "Ferharri";
+        // c1.color = "red";
 
-        System.out.println(x.name);
+        // System.out.println(x.name);
+
+        // Creating object with Constructor
+        Student a = new Student("manish", 13, 95.5);
+        a.percent = 95.23;
+        System.out.println(Student.getNoOfStudents());
+        // Student b = new Student("Grishma", 13, 91);
+        Student b = new Student(); // we didn't pass any arguments, and it works too because we made a default
+                                   // constructor in Student.java package
+        b.name = "Grishma";
+        System.out.println(Student.getNoOfStudents());
+        System.out.println(b.school);
     }
 }
