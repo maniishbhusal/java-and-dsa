a38f445403489209b15da722f6cf6ddaf371f917
Manish Bhusal <84217955+maniishbhusal@users.noreply.github.com>
2024-02-21 20:29:09 +0545

    Update FriendsPairing.java

--- 11. Recursion/FriendsPairing.java
+++ 11. Recursion/FriendsPairing.java
@@ -1,5 +1,6 @@
 /*Friends Pairing Problem
- Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. Find out the total number of ways in which friends can remain single or can be paired up.
+ Given n friends, each one can remain single or can be paired up with some other friend. Each friend can be paired only once. Find out the total number of 
+ ways in which friends can remain single or can be paired up.
  */
 
 public class FriendsPairing {
@@ -25,9 +26,12 @@ public class FriendsPairing {
     Keep the first friend single and solve the problem for the remaining n-1 friends.
     Pair the first friend with one of the remaining (n-1) friends and solve the problem for the remaining (n-2) friends.
 
-    (n-1): This part represents the number of ways the first friend can pair up with one of the remaining (n-1) friends. The first friend has (n-1) options to choose a partner from the remaining friends.
+    (n-1): This part represents the number of ways the first friend can pair up with one of the remaining (n-1) friends. 
+    The first friend has (n-1) options to choose a partner from the remaining friends.
 
-    f(n-2): This part represents the total number of ways the remaining (n-2) friends can be either single or paired up among themselves. Since the first friend has already been paired up with one of the remaining friends, we need to solve the problem for the remaining (n-2) friends.
+    f(n-2): This part represents the total number of ways the remaining (n-2) friends can be either single or paired up among themselves. 
+    Since the first friend has already been paired up with one of the remaining friends, we need to solve the problem for the remaining (n-2) friends.
 
-    So, combining these two parts, "(n-1) * f(n-2)" gives the total number of ways the first friend can pair up with one of the remaining friends, while considering all possible arrangements for the remaining friends.
+    So, combining these two parts, "(n-1) * f(n-2)" gives the total number of ways the first friend can pair up with one of the remaining friends, 
+    while considering all possible arrangements for the remaining friends.
  */
