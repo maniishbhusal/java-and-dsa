d3d878f48a2c26b8a61c912df2ff38ac496913cd
Manish Bhusal <manishbhusal6@gmail.com>
2024-02-29 16:31:30 +0545

    solve N Queens problem

---
+++ 14. Backtracking/NQueens.java
@@ -0,0 +1,72 @@
+// Place N queens on an NxN chessboard such that no 2 queens can attack each
+// other
+
+public class NQueens {
+
+    public static void nQueens(char board[][], int row) {
+
+        // base case
+        if (row == board.length) {
+            printBoard(board);
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1);
+                board[row][j] = 'x'; // backtracking
+            }
+        }
+    }
+
+    public static boolean isSafe(char board[][], int row, int col) {
+
+        // vertical up
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal left
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // diagonal right
+        for (int i = row - 1, j = col + 1; i >= 0 && j <= board.length - 1; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("--------Chess Board--------");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+
+        // initialize
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+
+        nQueens(board, 0);
+    }
+}
